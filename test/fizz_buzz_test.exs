a2f6a14e8eab75be67bafe378861dfb2b2b60291
Leonardo Gonçalves <leogonsil97@gmail.com>
2020-10-10 03:22:18 -0300

    unit tests

--- test/fizz_buzz_test.exs
+++ test/fizz_buzz_test.exs
@@ -1,8 +1,13 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "build/1" do
+    test "when a valid file is provided, returns the converted list" do
+      assert FizzBuzz.build("numbers.txt") == {:ok, [1, 2, :buzz, :buzz, :buzz]}
+    end
+
+    test "when an invalid file is provided, returns an error" do
+      assert FizzBuzz.build("invalid.txt") == "File reading error. Reason: enoent"
+    end
   end
 end
